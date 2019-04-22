e7c24c851db170029ae8894145d9c8fb12401b6b
hewking <604910070@qq.com>
2019-04-22 09:57:10 +0800

    update

--- src/component/ResponderTestScreen.js
+++ src/component/ResponderTestScreen.js
@@ -9,97 +9,79 @@ import {Text
 
 export default class extends Component {
 
-    constructor(){
-        super()
+    constructor(props) {
+        super(props);
         this.state = {
-            bg : 'red'
+            redViewBgColor: 'red',
+            marginTop: 100,
+            marginLeft: 100,
         }
     }
 
-    render(){
-        return (<View style={styles.container}>
-                <View 
-                {...this._panResponder}
-                    style={[styles.rect,{backgroundColor:this.state.bg}]}
-                >
-                <View
-                    {...this._gestureHandlers}
-                 style={{backgroundColor:'skyblue',width:100,height:100}}/>
-            </View>
-        </View>)
-    }
-
-    componentDidMount(){
-        this._gestureHandlers = {
-            onStartShouldSetResponder: ()=> true,
-            onMoveShouldSetResponder : () => true,
-            onResponderGrant : () => {
-                console.log('onResponderGrant')
-                this.setState({bg:'red'})
-            },
-            onResponderMove : () => {
-                console.log('onResponderMove')
-                this.setState({bg:'green'})
-            },
-            onResponderRelease : () => {
-                  console.log('onResponderMove')
-                this.setState({bg:'skyblue'})
-            }
-        }
-
+    componentWillMount(){
         this._panResponder = PanResponder.create({
-            // 要求成为响应者：
             onStartShouldSetPanResponder: (evt, gestureState) => {
-                console.log('onStartShouldSetPanResponder')
-                return true},
-            onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
-            onMoveShouldSetPanResponder: (evt, gestureState) => true,
-            onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
-      
+                return true;
+            },
+            onMoveShouldSetPanResponder:  (evt, gestureState) => {
+                return true;
+            },
             onPanResponderGrant: (evt, gestureState) => {
-              // 开始手势操作。给用户一些视觉反馈，让他们知道发生了什么事情！
-      
-              // gestureState.{x,y} 现在会被设置为0
-              console.log('onPanResponderGrant')
+                this._highlight();
             },
             onPanResponderMove: (evt, gestureState) => {
-              // 最近一次的移动距离为gestureState.move{X,Y}
-      
-              // 从成为响应者开始时的累计手势移动距离为gestureState.d{x,y}
-              console.log('onPanResponderMove')
-            },
-            onPanResponderTerminationRequest: (evt, gestureState) => true,
-            onPanResponderRelease: (evt, gestureState) => {
-              // 用户放开了所有的触摸点，且此时视图已经成为了响应者。
-              // 一般来说这意味着一个手势操作已经成功完成。
-              console.log('onPanResponderRelease')
+                // console.log(`locationX : ${evt.nativeEvent.locationX}   locationY : ${evt.nativeEvent.locationY}`);
+                // this.setState({
+                //         marginLeft: evt.nativeEvent.locationX,
+                //         marginTop: evt.nativeEvent.locationY,
+                // });
 
+                console.log(`pageX : ${evt.nativeEvent.pageX}   pageY : ${evt.nativeEvent.pageY}`);
+                this.setState({
+                        marginLeft: evt.nativeEvent.pageX ,
+                        marginTop: evt.nativeEvent.pageY,
+                });
+            },
+            onPanResponderRelease: (evt, gestureState) => {
+                this._unhighlight();
             },
             onPanResponderTerminate: (evt, gestureState) => {
-              // 另一个组件已经成为了新的响应者，所以当前手势将被取消。
-              console.log('onPanResponderTerminate')
             },
-            onShouldBlockNativeResponder: (evt, gestureState) => {
-              // 返回一个布尔值，决定当前组件是否应该阻止原生组件成为JS响应者
-              // 默认返回true。目前暂时只支持android。
-              return true;
-            },
-          });
+        });
+    }
 
+    _unhighlight(){
+        this.setState({redViewBgColor: 'red'})
+    }
+
+    _highlight(){
+        this.setState({redViewBgColor: 'blue'})
+    }
+
+    render() {
+        return (
+            <View style={styles.container}  {...this._panResponder.panHandlers}>
+                <View style={[styles.redView,{backgroundColor: this.state.redViewBgColor
+                ,marginTop: this.state.marginTop,
+                marginLeft: this.state.marginLeft,}]}
+                   
+                ></View>
+            </View>
+        );
     }
 
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        backgroundColor:'skyblue'
     },
-    rect : {
-        flex:1,
-        backgroundColor:'red',
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
+    redView: {
+        width: 100,
+        height: 100,
+        marginTop: 100,
+        marginLeft: 100,
+    },
+
+});
